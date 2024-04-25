ff8fa8a4f84d5b1ab3e3c2b5fa51b366a6d01862
SOMY@ <79443987+Assassin69420@users.noreply.github.com>
2024-04-25 18:36:32 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@
 const colors = require("tailwindcss/colors");
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./global.css",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
