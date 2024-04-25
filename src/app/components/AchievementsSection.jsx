2665016000110331afff2860c20c5a3dcf5051f1
Assassin69420 <sunnygajraj11@gmail.com>
2024-04-25 20:31:48 +0530

    deploy commit

--- src/app/components/AchievementsSection.jsx
+++ src/app/components/AchievementsSection.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import dynamic from "next/dynamic";
 import TabButton from "./TabButton";
 
-
 const AnimatedNumbers = dynamic(
   () => {
     return import("react-animated-numbers");
@@ -69,66 +68,3 @@ const AchievementsSection = () => {
 };
 
 export default AchievementsSection;
-
-
-
-
-
-<div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-<section className="text-white" id="about">
-  <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-    <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
-      <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
-      <p className="text-base lg:text-lg">
-        I am a full stack web developer with a passion for creating
-        interactive and responsive web applications. I have experience
-        working with JavaScript, React, Redux, Node.js, Express, MongoDB,
-        Bootstrap, HTML, CSS and GitHub. I am a quick learner and I am always
-        looking to expand my knowledge and skill set. I am a team player and
-        I am excited to work with others to create amazing applications.
-      </p>
-  </div>    
-
-<div class="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-<div class="flex items-center justify-between mb-0">
-</div>
-<div class="flow-root">
-  <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
-        <li class="py-3 sm:py-4">
-            <div class="flex items-center">
-            </div>
-        </li>
-    </ul>
-        
-</div>
-
-      <div className="flex flex-row justify-center mt-8">
-        <TabButton
-          selectTab={() => handleTabChange("skills")}
-          active={tab === "skills"}
-        >
-          {" "}
-          Skills{" "}
-        </TabButton>
-        <TabButton
-          selectTab={() => handleTabChange("education")}
-          active={tab === "education"}
-        >
-          {" "}
-          Education{" "}
-        </TabButton>
-        <TabButton
-          selectTab={() => handleTabChange("certifications")}
-          active={tab === "certifications"}
-        >
-          {" "}
-          Certifications{" "}
-        </TabButton>
-      </div>
-      <div className="mt-8">
-        {TAB_DATA.find((t) => t.id === tab).content}
-      </div>
-    </div>
-  </div>
-</section>
-</div>
