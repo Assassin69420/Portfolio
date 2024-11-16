e89403efce3740decb3132d944f74261f62144c7
SOMY@ <79443987+Assassin69420@users.noreply.github.com>
2024-11-16 12:31:11 +0530

    Update HeroSection.jsx
    noe it's perfect

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -7,8 +7,8 @@ import Link from "next/link";
 
 const HeroSection = () => {
   return (
-    <section className="lg:py-16">
-      <div className="grid grid-cols-1 sm:grid-cols-18">
+    <section className="lg:py-16 mb-28" >
+      <div className="grid grid-cols-1 sm:grid-cols-12">
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
@@ -34,8 +34,7 @@ const HeroSection = () => {
               repeat={Infinity}
             />
           </h1>
-          <Image src="/images/my_pick1.png" height={300} width={300} alt="My Picture"/>
-          <div>
+          <div> 
             <Link
               href="/#contact"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
@@ -58,6 +57,15 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/my_pick1.png"
+              alt="hero image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
+          </div>
         </motion.div>
       </div>
     </section>
